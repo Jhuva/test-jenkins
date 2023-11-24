5ade0317a87e36d48b26576777004fde9ab5e8c6
Jhuvael Zambrano <Jhuvael Zambrano@unibanca.com.pe>
2023-11-24 18:31:59 -0500

    Mas cambios en prod

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola SBO está usando jenkins! en PRODUCCION");
+        System.out.println("Hola SBO está usando jenkins! en PRODUCCION 222");
     }
 }
